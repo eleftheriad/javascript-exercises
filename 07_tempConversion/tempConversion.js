6b895ffc56797826e739eaf6bb189fca70a53b0c
petros11888 <petroseleftheriadis8@gmail.com>
2022-05-17 18:52:39 +0300

    done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp_in_F) {
+  let temp_in_C = (temp_in_F - 32) * 5/9;
+  temp_in_C = Math.round(temp_in_C * 10) / 10
+  return temp_in_C
 };
 
-const ctof = function() {
-
+const ctof = function(temp_in_C) {
+  let temp_in_F = (temp_in_C * 9/5) + 32;
+  temp_in_F = Math.round(temp_in_F * 10) / 10
+  return temp_in_F
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x+y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum_= 0
+	for (let i = 0; i < array.length; i++) {
+    sum_ = sum_ + array[i];
+  }
+  return sum_
 };
 
-const multiply = function() {
-
+const multiply = function(Y) {
+  let product = 1;
+  for (let i = 0; i < Y.length; i++) {
+    product = product * Y[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(x,y);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if (x === 0 || x===1) {
+    return 1;
+  }
+  let fact = x;
+  for (let i = 0; i < x-1 ; i++) {
+    fact = fact * (x-1-i)
+  }
+  return fact;
 };
 
 // Do not edit below this line
