60bb3a79a0a7de848df1e8b332340f1e6b279d3e
petros11888 <petroseleftheriadis8@gmail.com>
2022-05-17 15:39:05 +0300

    1 & 2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    if(num<0){
+        return 'ERROR'
+    }
+    let new_word='';
+    for (let i = 0; i < num; i++) {
+        new_word = new_word + word        
+    }
+    return(new_word)
 };
-
+repeatString("hey", 3);
 // Do not edit below this line
 module.exports = repeatString;
